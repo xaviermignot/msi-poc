f3a2ca59565d6f19c1f3422e9d49ecb481603ede
Xavier Mignot <Xavier.Mignot@azeo.com>
2020-08-04 15:25:23 +0200

    Return content

--- src/MsiPoc.SesApi/GetDeviceFunction.cs
+++ src/MsiPoc.SesApi/GetDeviceFunction.cs
@@ -8,6 +8,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using MsiPoc.Abstractions;
 
 namespace MsiPoc.SesApi
 {
@@ -31,9 +32,10 @@ namespace MsiPoc.SesApi
             ILogger log)
         {
             var response = await _httpClient.GetAsync(new Uri(_dpsConfiguration.BaseUri, $"devices/{id}"));
+
             return response.StatusCode switch
             {
-                HttpStatusCode.OK => new OkResult(),
+                HttpStatusCode.OK => new OkObjectResult(await response.Content.ReadAsAsync<Device>()),
                 HttpStatusCode.NotFound => new NotFoundResult(),
                 HttpStatusCode.RedirectKeepVerb => new RedirectResult(response.Headers.Location.ToString()),
                 _ => throw new NotSupportedException()
